e34f86cbbd7b809d1cc8107b940b3536d5dff03e
Vitaliy <vitalyazems@gmail.com>
2024-11-18 19:20:14 +0500

    Изменил конфигурацию app.js

--- api/app.js
+++ api/app.js
@@ -1,4 +1,20 @@
-const { app } = require('../backend/app');
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use('/api', routes);
+
+mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
+  console.log(`Connected to MongoDB`);
+});
 
 module.exports = (req, res) => {
   app(req, res);
