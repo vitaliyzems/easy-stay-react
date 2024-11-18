016f0d64695281962701d3db869d7eadafca35ca
Vitaliy <vitalyazems@gmail.com>
2024-11-18 19:14:27 +0500

    Деплой на Vercel

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 
-const port = 8888;
+// const port = 8888;
 const app = express();
 
 app.use(express.static('../frontend/build'));
@@ -15,14 +15,16 @@ app.use(express.json());
 
 app.use('/api', routes);
 
-mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
-  app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-  });
-});
-
 // mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
-//   console.log(`Connected to MongoDB`);
+//   app.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+//   });
 // });
 
-module.exports = { app };
+mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
+  console.log(`Connected to MongoDB`);
+});
+
+module.exports.app = (req, res) => {
+  app(req, res);
+};
