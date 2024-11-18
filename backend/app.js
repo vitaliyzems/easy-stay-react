7691d7d1ebb14679a7e112be28c53ccd919dcdac
Vitaliy <vitalyazems@gmail.com>
2024-11-18 22:22:24 +0500

    Ready to deploy

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,35 @@
-require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
+const path = require('path');
+
+require('dotenv').config();
 
 // const port = 8888;
 const app = express();
-
-app.use(express.static('../frontend/build'));
 
 app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api', routes);
 
-mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
-  app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
   });
-});
+}
+
+// mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
+//   app.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+//   });
+// });
 
 mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
   console.log(`Connected to MongoDB`);
 });
 
-module.exports.app = (req, res) => {
-  app(req, res);
-};
+module.exports = app;
